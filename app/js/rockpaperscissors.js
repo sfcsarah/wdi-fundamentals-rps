6198ea560a58d6bb3d8c188792bfb399d0abb889
sfcsarah <sfc.sarah@gmail.com>
2016-01-17 22:30:06 -0800

    end of part 6, 1st try

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -2,10 +2,10 @@
 /*   Provided Code - Please Don't Edit   */
 ////////////////////////////////////////////////
 'use strict';
-
+var move;
 function getInput() {
     console.log("Please choose either 'rock', 'paper', or 'scissors'.")
-    return prompt();
+    var move = return prompt();
 }
 function randomPlay() {
     var randomNumber = Math.random();
@@ -36,8 +36,8 @@ function getComputerMove(move) {
     return move || randomPlay()/* Your Expression */;
 }
 
+var winner;
 function getWinner(playerMove,computerMove) {
-    var winner;
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
@@ -60,12 +60,12 @@ function getWinner(playerMove,computerMove) {
     return winner;
 }
 
+var playerWins = 0;
+var computerWins = 0;
+var round = 0;
+var message = "";
 function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
-    var playerWins = 0;
-    var computerWins = 0;
-    var round = 0;
-    var message = "";
     while (playerWins < 5 && computerWins < 5) {
       var winner = getWinner(getPlayerMove(move),getComputerMove(move));
       if (winner === 'player') {
